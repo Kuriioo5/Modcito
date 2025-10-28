320a3f42608be7fcb385b8a0e11c30439d2feee6
Kuriioo5 <gilbertmcdavid@gmail.com>
2025-10-28 18:17:55 +0100

    Init

---
+++ src/main/java/net/modcito/ModcitoDataGenerator.java
@@ -0,0 +1,11 @@
+package net.modcito;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ModcitoDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
