9a707e2043625dc6c59e99bf5c4be7c40b6dd51f
Kuriioo5 <gilbertmcdavid@gmail.com>
2025-10-30 23:12:09 +0100

    Fix indent

--- src/main/java/net/modcito/Modcito.java
+++ src/main/java/net/modcito/Modcito.java
@@ -7,8 +7,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Modcito implements ModInitializer {
-	public static final String MOD_ID = "modcito";
 
+	public static final String MOD_ID = "modcito";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
